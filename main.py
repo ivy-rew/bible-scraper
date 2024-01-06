e510fd2d6c828dec55ddba0290b0fb9bec4dc3e9
LordEagle84 <r.wermelinger@gmail.com>
2024-01-06 18:00:30 +0100

    comply with current gateway impl

--- main.py
+++ main.py
@@ -1,28 +1,60 @@
 #!/usr/bin/env python3
-import os
 import requests
+import sys
 from contextlib import closing
 from bs4 import BeautifulSoup
 
+
+
 base_url = "https://www.biblegateway.com/passage/?search="
 
-book = str(input("Please enter the book of the Bible to scrape.\n"))
-translation = str(input("Please enter the translation to use.\n")).upper()
-chapter_num = 1
-verse_num = 1
+#translation = str(input("Please enter the translation to use.\n")).upper()
+translation = "SCH2000"
 
-if not os.path.exists("Bible"):
-    os.mkdir("Bible")
-if not os.path.exists("Bible/" + translation):
-	os.mkdir("Bible/" + translation)
-if not os.path.exists("Bible/" + translation + "/" + book):
-	os.mkdir("Bible/" + translation + "/" + book)
+if len(sys.argv) > 1:
+    book = sys.argv[1];
+else:
+    book = str(input("Please enter a the book.\n"))
 
-full_url = base_url + book + "+" + str(chapter_num) + "&version=" + translation
+if len(sys.argv) > 2:
+    chapter_num = sys.argv[2];
+else:
+    chapter_num = str(input("Please enter a chapter no.\n"))
+
+if len(sys.argv) > 3:
+    verse_num = sys.argv[3];
+else:
+    verse_num = str(input("Please enter a verse to fetch.\n"))
+
+ref=chapter_num;
+ref=ref+ ":" +verse_num
+
+full_url = base_url + book + "+" + ref + "&version=" + translation
 
 page = requests.get(full_url)
-soup = BeautifulSoup(page.text, "html.parser")
+soup = BeautifulSoup(page.text, "lxml")
 
-verse = soup.find(class_="text" + book + "-" + str(chapter_num) + "-" + str(verse_num))
-verse_list = soup.find(class_="BodyText")
-verse_list_contents = verse_list.find_all("a")
+foot = soup.findAll(class_="footnotes")
+[line.extract() for line in foot]
+
+h2 = soup.findAll("h2")
+[line.extract() for line in h2]
+
+h3 = soup.findAll("h3")
+[line.extract() for line in h3]
+
+sup = soup.findAll("sup")
+[note.extract() for note in sup]
+
+chapNos = soup.findAll(class_="chapternum")
+[no.extract() for no in chapNos]
+
+link = soup.find_all(class_="full-chap-link")
+[l.extract() for l in link]
+
+result = soup.find(class_="text-html");
+
+#print("verse="+str(result.prettify()))
+
+print(str(result.text.strip()))
+print(book+" "+ref)
