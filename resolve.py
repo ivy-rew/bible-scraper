d4856b2e50246983d2eaf7af312304bd9a514e5d
LordEagle84 <r.wermelinger@gmail.com>
2024-01-10 22:08:38 +0100

    pointer to re

--- resolve.py
+++ resolve.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 from pathlib import Path
+import re
 
 md = sys.argv[1]
 f = open(md, "r")
@@ -12,6 +13,12 @@ def bookOfFile(file):
 book = bookOfFile(md)
 print("book="+book)
 
+# https://docs.python.org/3/library/re.html
+m = re.match(r"(\w+) (\w+)", "Isaac Newton, physicist")
+m.group(0)       # The entire match
+m.group(1)       # The first parenthesized subgroup.
+m.group(2)       # The second parenthesized subgroup.
+m.group(1, 2)    # Multiple arguments give us a tuple.
 
 
 lines = f.readlines()
