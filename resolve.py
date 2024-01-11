780f908686ea239fb0c93b163b0d25a57c2890b3
LordEagle84 <r.wermelinger@gmail.com>
2024-01-11 20:08:23 +0100

    full text replacer

--- resolve.py
+++ resolve.py
@@ -3,32 +3,46 @@ import sys, re
 from pathlib import Path
 import gateway
 
-md = sys.argv[1]
-f = open(md, "r")
 
 def bookOfFile(file):
     mdNoExt=Path(file).stem
     return mdNoExt.split("-",1)[1]
 
-book = bookOfFile(md)
-print("book="+book)
+def verseInject(m):
+    ref=m.group(1)+":"+m.group(3)
+    quote=gateway.lookup(book, ref)
+    indent = "\n    > "
+    mdQuote = indent+str(quote)+indent+book.capitalize()+" "+ref
+    return m.group(1)+m.group(2) + "\n" + mdQuote
 
+def expandVerse(line):
+    return re.sub("([0-9])+(\\. .+) !V([0-9\\-]+)", verseInject, line, flags=re.IGNORECASE)
 
 def findRef(line):
     # https://docs.python.org/3/library/re.html
-    m = re.match("([0-9])+\\. .+ !V([0-9\\-]+)", line) #"1. test !V19"
+    m = re.match("(([0-9])+\\. .+) !V([0-9\\-]+)", line) #"1. test !V19"
     if m:
         ref=m.group(1)+":"+m.group(2)
         return ref;
 
-lines = f.readlines()
-for line in lines:
-    ref = findRef(line)
-    if ref:
-        quote=gateway.lookup(book, ref)
-        indent = "\n    > "
-        mdQuote = indent+str(quote)+indent+book.capitalize()+" "+ref
-        print(line + mdQuote)
-    else:
+def mdRefsReplace(mdPath):
+    with open(mdPath, 'r') as file:
+        lines = []
+        for line in file.readlines():
+            lines.append(expandVerse(line))
+        return lines
+
+def write(mdPath, lines):
+    with open(mdPath, 'w', encoding='utf-8') as file: 
+        file.writelines(lines) 
+
+#print(expandVerse('1. test !V19'))
+mdPath = sys.argv[1]
+book = bookOfFile(mdPath)
+lines = mdRefsReplace(mdPath)
+
+if len(sys.argv) > 2 and sys.argv[2] == "-i":
+    write(mdPath,lines)
+else:
+    for line in lines:
         print(line)
-    #print(line)
