2d59e737cef4573a4c18f0cfe525c7aef1bdd43b
LordEagle84 <r.wermelinger@gmail.com>
2024-02-16 21:45:52 +0100

    allow refs to other books; requiring full verse spec

--- resolve.py
+++ resolve.py
@@ -12,20 +12,26 @@ def bookOfFile(file):
     return mdNoExt.split("-",1)[1]
 
 def verseInject(m):
-    ref=m.group(1)+":"+m.group(4)
-    quote=gateway.lookup(book, ref)
+    verseBook = book
+    ref=m.group(1)+":"+m.group(5)
+    if m.group(4): # full-reference to other book
+        fullRefMatcher = re.match("([0-9_]*[A-za-z]+)([0-9]+):", str(m.group(4)))
+        if fullRefMatcher:
+            verseBook = fullRefMatcher.group(1)
+            ref = fullRefMatcher.group(2)+":"+m.group(5)
+    quote=gateway.lookup(verseBook, ref)
     if quote is None:
-        print("gateway lookup failed for "+book+" "+ref)
+        print("gateway lookup failed for "+verseBook+" "+ref)
         return m.group(0)
     indent = "   > "
-    if m.group(3) == '!!':
+    if m.group(3) == '!!': # full-quote; inlined
         br = "  "
         indent = " " + indent # 4whitspaces: in-between list content!
-        mdQuote = indent+str(quote)+br+"\n"+book.capitalize()+" "+ref
+        mdQuote = indent+str(quote)+br+"\n"+verseBook.capitalize()+" "+ref
         return m.group(1)+m.group(2)+ '\n\n' + mdQuote + br + '\n'
-    else:
-        foot = "[^"+book+ref+"]"
-        mdQuote = indent+str(quote)+"  "+"\n"+book.capitalize()+" "+ref
+    else: # footnote
+        foot = "[^"+verseBook+ref+"]"
+        mdQuote = indent+str(quote)+"  "+"\n"+verseBook.capitalize()+" "+ref
         notes.append(foot+":"+mdQuote+"\n")
         return m.group(1)+m.group(2) + foot
 
@@ -34,7 +40,7 @@ def expandVerse(line):
     matched = True
     while matched:
         incoming = replaced
-        replaced = re.sub("([0-9]+)(\\. [^!]+)(\\!+)V([0-9\\-]+)", verseInject, incoming, flags=re.IGNORECASE)
+        replaced = re.sub("^([0-9]+)(\\. [^!]+)(\\!+)V([_0-9A-Za-z]+:)?([0-9\\-]+)", verseInject, incoming, flags=re.IGNORECASE | re.MULTILINE)
         matched = replaced != incoming
     return replaced
 
--- tests/test_resolve.py
+++ tests/test_resolve.py
@@ -23,6 +23,12 @@ class TestMarkdownResolver(unittest.TestCase):
         print(resolve.notes[0])
         self.assertEqual(resolve.notes[0], '[^Genesis1:1]:   > Im Anfang schuf Gott die Himmel und die Erde.  \nGenesis 1:1\n')
 
+    def test_injectFootnotes_fullRefOtherBook(self):
+        resolve.notes = []
+        expaned = resolve.expandVerse('1. Himmel !VExodus30:12 und !V2. Erde !V1')
+        self.assertEqual(expaned, '1. Himmel [^Exodus30:12] und [^Genesis1:2]. Erde [^Genesis1:1]')
+        print(resolve.notes[0])
+
     def test_fullQuote(self):
         resolve.notes = []
         expaned = resolve.expandVerse('1. Himmel und Erde !!V1')
