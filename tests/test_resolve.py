37dcb15ae4ad54a204f116c75f691561603e2c21
LordEagle84 <r.wermelinger@gmail.com>
2024-04-02 17:05:45 +0200

    fix: mocked requests; offlien capable

--- tests/test_resolve.py
+++ tests/test_resolve.py
@@ -1,36 +1,37 @@
 import unittest
 import scraper.resolve as resolve
+import scraper.MdExpand as MdExpand
 from unittest.mock import Mock
 
-resolve = resolve;
-resolve.gateway = Mock()
-resolve.MdExpand.book = 'Genesis';
-resolve.gateway.lookup.return_value = 'Im Anfang schuf Gott die Himmel und die Erde.'
+exDef = MdExpand
+exDef.MdExpand.book = 'Genesis';
+exDef.gateway = Mock()
+exDef.gateway.lookup.return_value = 'Im Anfang schuf Gott die Himmel und die Erde.'
 
 class TestMarkdownResolver(unittest.TestCase):
 
     def test_injectFootnotes_single(self):
-        expander = resolve.MdExpand()
+        expander = exDef.MdExpand()
         expaned = expander.expandVerse('1. Himmel und Erde !V1')
         self.assertEqual(expaned, '1. Himmel und Erde [^Genesis1:1]')
         print(expander.notes[0])
         self.assertEqual(expander.notes[0], '[^Genesis1:1]:   > Im Anfang schuf Gott die Himmel und die Erde.  \nGenesis 1:1\n')
 
     def test_injectFootnotes_multiple(self):
-        expander = resolve.MdExpand()
+        expander = exDef.MdExpand()
         expaned = expander.expandVerse('1. Himmel und Erde !V1 !V3')
         self.assertEqual(expaned, '1. Himmel und Erde [^Genesis1:1] [^Genesis1:3]')
         print(expander.notes[0])
         self.assertEqual(expander.notes[0], '[^Genesis1:1]:   > Im Anfang schuf Gott die Himmel und die Erde.  \nGenesis 1:1\n')
 
     def test_injectFootnotes_fullRefOtherBook(self):
-        expander = resolve.MdExpand()
+        expander = exDef.MdExpand()
         expaned = expander.expandVerse('1. Himmel !VExodus30:12 und !V2. Erde !V1')
         self.assertEqual(expaned, '1. Himmel [^Exodus30:12] und [^Genesis1:2]. Erde [^Genesis1:1]')
         print(expander.notes[0])
 
     def test_fullQuote(self):
-        expander = resolve.MdExpand()
+        expander = exDef.MdExpand()
         expaned = expander.expandVerse('1. Himmel und Erde !!V1')
         self.assertEqual(expaned, '1. Himmel und Erde \n\n    > Im Anfang schuf Gott die Himmel und die Erde.  \nGenesis 1:1  \n')
 
