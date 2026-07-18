82390bb93af73da67bda5b93331bf68a0809078a
LordEagle84 <r.wermelinger@gmail.com>
2026-07-18 21:05:09 +0200

    fix test: render

--- tests/test_gateway.py
+++ tests/test_gateway.py
@@ -6,7 +6,7 @@ class TestHtmlParser(unittest.TestCase):
     def test_gateway_parse(self):
         with open('tests/genesis1-1.html', 'r') as file:
             html = file.read(); #.replace('\n', '')
-        result = scraper.gateway.toPlainText(html)
+        result = scraper.gateway.render(html, True)
         self.assertEqual(result, 'Im Anfang schuf Gott die Himmel und die Erde.')
 
 if __name__ == '__main__':
